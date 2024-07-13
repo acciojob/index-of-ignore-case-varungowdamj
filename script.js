98343a8d63efebb52f6140afc38d3324584abcc5
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2024-07-13 22:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+            // Convert both strings to lower case to perform a case-insensitive search
+            let lowerS1 = s1.toLowerCase();
+            let lowerS2 = s2.toLowerCase();
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+            // Use the built-in indexOf method to find the index of the substring
+            return lowerS1.indexOf(lowerS2);
+        }
+
+        const s1 = prompt("Enter s1:");
+        const s2 = prompt("Enter s2:");
+        alert(indexOfIgnoreCase(s1, s2));
